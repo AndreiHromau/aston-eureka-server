95d796a8e6231f5ce743f89d59333e1391730142
AndreiHromau <Andreihromau@gmail.com>
2026-04-29 17:31:27 +0300

    chore: cоздан spring boot проект для сервера Eureka

---
+++ src/main/java/ru/aston/astoneurekaserver/AstonEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.astoneurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonEurekaServerApplication.class, args);
+    }
+
+}
