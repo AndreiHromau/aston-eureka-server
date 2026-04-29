31036533cc2551f50a759aa17e4c3841bc46d8d7
AndreiHromau <Andreihromau@gmail.com>
2026-04-29 17:38:14 +0300

    feat: добавлены @EnableEurekaServer на точку входа и поправлен application.properties

--- src/main/java/ru/aston/astoneurekaserver/AstonEurekaServerApplication.java
+++ src/main/java/ru/aston/astoneurekaserver/AstonEurekaServerApplication.java
@@ -2,7 +2,9 @@ package ru.aston.astoneurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class AstonEurekaServerApplication {
 
